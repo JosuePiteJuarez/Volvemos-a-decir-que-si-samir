f5056c5ae932c074a821025c64178fa510046b52
JosuePiteJuarez <137839387+JosuePiteJuarez@users.noreply.github.com>
2024-07-16 10:19:20 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function myFunction() {
+  var element = document.getElementById("anim2d");
+  element.classList.toggle("hidden");
+  var element = document.getElementById("anim2d");
+  element.classList.toggle("animate__flipInX");
+};
+
+function myFunction2() {
+  var element = document.getElementById("motiongraphics");
+  element.classList.toggle("hidden");
+  var element = document.getElementById("motiongraphics");
+  element.classList.toggle("animate__flipInX");
+};
+
+function myFunction3() {
+  var element = document.getElementById("anima3");
+  element.classList.toggle("hidden");
+  var element = document.getElementById("anima3");
+  element.classList.toggle("animate__flipInX");
+};
+function myFunction4() {
+  var element = document.getElementById("uxui");
+  element.classList.toggle("hidden");
+  var element = document.getElementById("uxui");
+  element.classList.toggle("animate__flipInX");
+};
